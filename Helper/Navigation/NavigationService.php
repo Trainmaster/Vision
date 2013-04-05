06edf5ca793775834c11fa7a59ce23da12cf7397
Trainmaster <trainmaster@gmx.net>
2013-04-05 13:21:29 +0200

    Added idb

--- Helper/Navigation/NavigationService.php
+++ Helper/Navigation/NavigationService.php
@@ -7,23 +7,57 @@ use SplFileObject;
 use RuntimeException;
 
 class NavigationService 
-{        
+{     
+    /**
+     * Node ID as root.
+     *
+     * @var null|int
+     */
     protected $rootId = null;
     
+    /**
+     * Language ID.
+     *
+     * @var null|int
+     */
     protected $languageId = null;
     
+    /**
+     * Reference to NavigationMapper instance.
+     *
+     * @var NavigationMapper
+     */
     protected $mapper = null;
     
+    /**
+     * Reference to Request instance.
+     *
+     * @var RequestInterface
+     */
     protected $request = null;
     
+    /**
+     * Reference to NavigationRenderer instance.
+     *
+     * @var NavigationRenderer
+     */
     protected $renderer = null;
-    
+     
+    /**
+     * @param object $mapper 
+     * @param object $renderer 
+     * 
+     * @return void
+     */
     public function __construct($mapper, $renderer)
     {
         $this->setMapper($mapper);
         $this->setRenderer($renderer);
     }
     
+    /**
+     * @return string 
+     */
     public function __toString()
     {
         $tree = $this->prepareTree();
@@ -32,41 +66,84 @@ class NavigationService
         return (string) $tree;
     }
     
+    /**
+     * Setter for $request property.
+     * 
+     * @param RequestInterface $request 
+     * 
+     * @return NavigationService Provides a fluent interface.
+     */
     public function setRequest(RequestInterface $request)
     {
         $this->request = $request;
         return $this;
     }
         
+    /**
+     * Setter for $rootId property.
+     * 
+     * @param int $id 
+     * 
+     * @return NavigationService Provides a fluent interface.
+     */
     public function setRootId($id)
     {
         $this->rootId = (int) $id;
         return $this;
     }
     
+    /**
+     * Setter for $languageId property.
+     * 
+     * @param int $id 
+     * 
+     * @return NavigationService Provides a fluent interface.
+     */
     public function setLanguageId($id)
     {
         $this->languageId = (int) $id;
         return $this;
     }
-    
+
+    /**
+     * Setter for $mapper property.
+     * 
+     * @param object $mapper 
+     * 
+     * @return NavigationService Provides a fluent interface.
+     */
     public function setMapper($mapper)
     {
         $this->mapper = $mapper;
         return $this;
     }
     
+    /**
+     * Setter for $renderer property.
+     * 
+     * @param NavigationRendererInterface $renderer 
+     * 
+     * @return NavigationService Provides a fluent interface.
+     */
     public function setRenderer(NavigationRendererInterface $renderer)
     {
         $this->renderer = $renderer;
         return $this;
     }
     
+    /**
+     * Prepares the tree.
+     * 
+     * @todo Implement caching mechanism (see inline comments).
+     *
+     * @throws RuntimeException
+     *
+     * @return array $tree
+     */
     protected function prepareTree()
     {
         /*
-        $cache = file_get_contents('XYZ.cache');
-        
+        $cache = file_get_contents('XYZ.cache');        
         if ($cache !== false) {
             return unserialize($cache);
         }
@@ -103,6 +180,13 @@ class NavigationService
         return $tree;        
     }
 
+    /**
+     * Converts flat array to hierarchical array.
+     * 
+     * @param array $data 
+     * 
+     * @return NavigationService Provides a fluent interface.
+     */
     protected function convertFlatToHierarchical(array $data)
     {
         foreach ($data as $id => $row) {
