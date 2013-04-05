616027171371bd3e86bb047de60ee0b82ed22836
Trainmaster <trainmaster@gmx.net>
2013-04-05 11:34:53 +0200

    Fixed cs

--- Helper/Navigation/NavigationMapper.php
+++ Helper/Navigation/NavigationMapper.php
@@ -8,45 +8,46 @@ class NavigationMapper
 {
     protected $pdo;
     
-	public function __construct(PDO $pdo)
+    public function __construct(PDO $pdo)
     {
-		$this->pdo = $pdo;
-	}	
-
-	public function loadByIdAndLanguageId($id, $languageId)
-    {	
-		$sql = 'SELECT 	nn.node_id, nn.show_link, nn.is_visible, nn.weight, nn.attributes,
-						nt2.ancestor AS parent, 
-						ni18n.name, ni18n.path
-				FROM navigation_tree as nt1
-					INNER JOIN navigation_node as nn
-						ON nn.node_id = nt1.descendant
-					LEFT JOIN navigation_tree as nt2
-						ON nt2.path_length = 1
-						AND nt2.descendant = nt1.descendant
-					LEFT JOIN navigation_node_i18n AS ni18n
-						ON ni18n.node_id = nn.node_id
-						AND ni18n.language_id = :language_id
-				WHERE nt1.ancestor = :ancestor
+        $this->pdo = $pdo;
+    }
+        
+    public function loadByIdAndLanguageId($id, $languageId)
+    {   
+        $sql = 'SELECT  nn.node_id, nn.show_link, nn.is_visible, nn.weight, nn.attributes,
+                    nt2.ancestor AS parent, 
+                    ni18n.name, ni18n.path
+                FROM navigation_tree as nt1
+                    INNER JOIN navigation_node as nn
+                        ON nn.node_id = nt1.descendant
+                    LEFT JOIN navigation_tree as nt2
+                        ON nt2.path_length = 1
+                        AND nt2.descendant = nt1.descendant
+                    LEFT JOIN navigation_node_i18n AS ni18n
+                        ON ni18n.node_id = nn.node_id
+                        AND ni18n.language_id = :language_id
+                WHERE nt1.ancestor = :ancestor
                 ORDER BY nn.weight ASC, ni18n.name ASC';
-		$pstmt = $this->pdo->prepare($sql);
-		$pstmt->bindParam(':ancestor', $id, PDO::PARAM_INT);
-		$pstmt->bindParam(':language_id', $languageId, PDO::PARAM_INT);
-		$pstmt->execute();
+                
+        $pstmt = $this->pdo->prepare($sql);
+        $pstmt->bindParam(':ancestor', $id, PDO::PARAM_INT);
+        $pstmt->bindParam(':language_id', $languageId, PDO::PARAM_INT);
+        $pstmt->execute();
 
-		$result = array();
-        
-		while ($data = $pstmt->fetch(PDO::FETCH_ASSOC)) {
-			$node = new Node($data['node_id']);
-			$node	->setShowLink($data['show_link'])
-					->setIsVisible($data['is_visible'])
-					->setParent($data['parent'])
-					->setName($data['name'])
-					->setPath($data['path'])
-					->setAttributes($data['attributes']);
-			$result[$data['node_id']] = $node;			
-		}
-        
-		return $result;
-	}
+        $result = array();
+
+        while ($data = $pstmt->fetch(PDO::FETCH_ASSOC)) {
+            $node = new Node($data['node_id']);
+            $node->setShowLink($data['show_link'])
+                ->setIsVisible($data['is_visible'])
+                ->setParent($data['parent'])
+                ->setName($data['name'])
+                ->setPath($data['path'])
+                ->setAttributes($data['attributes']);
+            $result[$data['node_id']] = $node;          
+        }
+
+        return $result;
+    }
 }
--- Helper/Navigation/Node.php
+++ Helper/Navigation/Node.php
@@ -4,120 +4,120 @@ namespace Vision\Helper\Navigation;
 class Node
 {
     protected $navigationId = null;
-	
-	protected $showLink = true;
-	
-	protected $isVisible = true;
-	
-	protected $parent = null;
-	
-	protected $name = null;
-	
-	protected $path = null;
-	
-	protected $attributes = array();
+    
+    protected $showLink = true;
+    
+    protected $isVisible = true;
+    
+    protected $parent = null;
+    
+    protected $name = null;
+    
+    protected $path = null;
+    
+    protected $attributes = array();
     
     protected $children = array();
 
-	public function __construct($navigationId)
+    public function __construct($navigationId)
     {
-		$this->setNavigationId($navigationId);
-	}
+        $this->setNavigationId($navigationId);
+    }
 
-	public function setNavigationId($navigationId)
+    public function setNavigationId($navigationId)
     {
-		$this->navigationId = (int) $navigationId;
-		return $this;
-	}
-	
-	public function getNavigationId()
+        $this->navigationId = (int) $navigationId;
+        return $this;
+    }
+    
+    public function getNavigationId()
     {
-		return $this->navigationId;
-	}
-	
-	public function setShowLink($showLink)
+        return $this->navigationId;
+    }
+    
+    public function setShowLink($showLink)
     {
-		$this->showLink = (bool) $showLink;
-		return $this;
-	}
-	
-	public function showLink()
+        $this->showLink = (bool) $showLink;
+        return $this;
+    }
+    
+    public function showLink()
     {
-		return $this->showLink;
-	}
-	
-	public function setIsVisible($isVisible)
+        return $this->showLink;
+    }
+    
+    public function setIsVisible($isVisible)
     {
-		$this->isVisible = (bool) $isVisible;
-		return $this;
-	}
-	
-	public function isVisible()
+        $this->isVisible = (bool) $isVisible;
+        return $this;
+    }
+    
+    public function isVisible()
     {
-		return $this->isVisible;
-	}
-	
-	public function setParent($parent)
+        return $this->isVisible;
+    }
+    
+    public function setParent($parent)
     {   
-		$this->parent = (int) $parent;
-		return $this;
-	}
-	
-	public function getParent()
+        $this->parent = (int) $parent;
+        return $this;
+    }
+    
+    public function getParent()
     {
-		return $this->parent;
-	}
+        return $this->parent;
+    }
     
     public function setChild($id, $child)
     {
-		$this->children[$id] = $child;
-	}
-	
-	public function getChildren()
+        $this->children[$id] = $child;
+    }
+    
+    public function getChildren()
     {
-		return $this->children;
-	}
-	
-	public function hasChildren()
+        return $this->children;
+    }
+    
+    public function hasChildren()
     {
-		return (bool) !empty($this->children);
-	}
-	
-	public function setName($name)
+        return (bool) !empty($this->children);
+    }
+    
+    public function setName($name)
     {
-		$this->name = (string) $name;
-		return $this;
-	}
-	
-	public function getName()
+        $this->name = (string) $name;
+        return $this;
+    }
+    
+    public function getName()
     {
-		return $this->name;
-	}
-	
-	public function setPath($path)
+        return $this->name;
+    }
+    
+    public function setPath($path)
     {
-		$this->path = (string) $path;
-		return $this;
-	}
-	
-	public function getPath()
+        $this->path = (string) $path;
+        return $this;
+    }
+    
+    public function getPath()
     {
-		return $this->path;
-	}
-	
-	public function setAttributes($attributes)
+        return $this->path;
+    }
+    
+    public function setAttributes($attributes)
     {
         if (isset($attributes)) {
             $attributes = json_decode($attributes, true);
             if ($attributes !== null) {
                 $this->attributes = $attributes;
-            }		
+            }       
         }
         return $this;
-	}
-	
-	public function getAttributes()
+    }
+    
+    public function getAttributes()
     {
-		return $this->attributes;
-	}
+        return $this->attributes;
+    }
 }
