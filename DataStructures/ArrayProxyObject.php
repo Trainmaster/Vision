29c75cbbf8dd75477729f0c8cb765904850bc362
Trainmaster <trainmaster@gmx.net>
2013-04-07 14:59:03 +0200

    Added idb + small fixes (method signature/name)

--- DataStructures/ArrayProxyObject.php
+++ DataStructures/ArrayProxyObject.php
@@ -1,38 +1,80 @@
 <?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
 namespace Vision\DataStructures;
 
 use ArrayAccess;
 
+/**
+ * @author Frank Liepert
+ */
 class ArrayProxyObject implements ArrayAccess
 {
+    /**
+     @type array $data
+     */
     protected $data = array();
         
-    public function __construct($data)
+    /**
+     * @param array $data 
+     * 
+     * @return void
+     */
+    public function __construct(array $data)
     {
         $this->data = $data;
     }
 
+    /**
+     * @param int|string $offset 
+     * 
+     * @return bool
+     */
     public function offsetExists($offset)
     {
         return isset($this->data[$offset]);
     }
 
+    /**
+     * @param int|string $offset
+     * 
+     * @return mixed
+     */
     public function &offsetGet($offset)
     {
         return $this->data[$offset];
     }
 
+    /**
+     * @param int|string $offset 
+     * @param mixed $value 
+     * 
+     * @return void
+     */
     public function offsetSet($offset, $value)
     {
         $this->data[$offset] = $value;
     }
 
+    /**
+     * @param int|string $offset 
+     * 
+     * @return void
+     */
     public function offsetUnset($offset)
     {
         unset($this->data[$offset]);
     }
     
-    public function toArray()
+    /**
+     * @return array
+     */
+    public function getArrayCopy()
     {
         return $this->data;
     }
