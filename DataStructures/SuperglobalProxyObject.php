b80a4208bd038bfffabdb600f2867285a1edc4de
Trainmaster <trainmaster@gmx.net>
2013-04-07 15:02:05 +0200

    Added idb +fixed cs

--- DataStructures/SuperglobalProxyObject.php
+++ DataStructures/SuperglobalProxyObject.php
@@ -1,30 +1,47 @@
 <?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+ 
 namespace Vision\DataStructures;
 
-class SuperglobalProxyObject
+/**
+ * @author Frank Liepert
+ */
+class SuperglobalProxyObject extends ArrayProxyObject
 {
-    protected $data = array();
-        
-    public function __construct($data)
-    {
-        $this->data = $data;
-    }
-
-    public function get($name)
+    /**
+     * Retrieve array value by html array notation
+     * 
+     * Example: $this->getValueByName(foo[bar][baz]) returns
+     *          the value of $this->data[$foo][$bar][$baz] or NULL.
+     * 
+     * @param string $name 
+     * 
+     * @return mixed|null
+     */
+    public function getValueByName($name)
     {
         if (strpos($name, '[]') !== false) {
             $name = str_replace('[]', '', $name);
         }
-		$parts = explode('[', $name);
-		$value = $this->data;
-		foreach ($parts as $part) {
-			$part = trim($part, ']');
-			if (isset($value[$part])) {
-				$value = $value[$part];
-			} else {
-				return null;
-			}
-		} 
-		return $value;
+        
+        $parts = explode('[', $name);
+        $value = $this->data;
+        
+        foreach ($parts as $part) {
+            $part = rtrim($part, ']');
+            if (isset($value[$part])) {
+                $value = $value[$part];
+            } else {
+                return null;
+            }
+        }
+        
+        return $value;
     }
 }
