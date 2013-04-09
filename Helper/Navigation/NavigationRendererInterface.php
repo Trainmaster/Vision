83ac1e4810b35d8017e63a0cd0bd5c7e570422a4
Trainmaster <trainmaster@gmx.net>
2013-04-09 18:20:29 +0200

    Added idb, changed method signature

--- Helper/Navigation/NavigationRendererInterface.php
+++ Helper/Navigation/NavigationRendererInterface.php
@@ -1,7 +1,22 @@
 <?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */ 
 namespace Vision\Helper\Navigation;
 
+/**
+ * @author Frank Liepert
+ */
 interface NavigationRendererInterface
 {
-    public function render($tree);
+    /**
+     * @param array $tree 
+     * 
+     * @return string
+     */
+    public function render(array $tree);
 }
