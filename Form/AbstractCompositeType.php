79a79391bcf5af9646bb62058a768898015600aa
Trainmaster <trainmaster@gmx.net>
2013-05-07 19:46:26 +0200

    dot operator already converts to string
    added idb, fixed cs

--- Form/AbstractCompositeType.php
+++ Form/AbstractCompositeType.php
@@ -17,26 +17,42 @@ use Vision\Html\Element as HtmlElement;
  */ 
 abstract class AbstractCompositeType extends HtmlElement
 {
+    /** @type null|string $name */
     protected $name = null;
     
+    /** @type array $elements */
     protected $elements = array();    
     
+    /** @type array $decorators */
     protected $decorators = array();
     
+    /**
+     * Constructor
+     * 
+     * @param string $name 
+     * 
+     * @return void
+     */
     final public function __construct($name) 
     {
         $this->name = trim($name);
         $this->init();
     }     
     
+    /**
+     * @return void
+     */
     abstract public function init();
     
+    /**
+     * @return string
+     */
     public function getContent() 
     {
         $content = '';
         
         foreach ($this->elements as $element) {
-            $content .= (string) $element;
+            $content .= $element;
         }
         
         foreach ($this->decorators as $decorator) {
@@ -47,68 +63,89 @@ abstract class AbstractCompositeType extends HtmlElement
         return $content;
     }
     
+    /**
+     * @param <type> $element 
+     * 
+     * @return \Vision\Form\AbstractCompositeType Provides a fluent interface.
+     */
     public function addElement($element) 
     {
         $this->elements[] = $element;
         return $this;
     }
     
+    /**
+     * @param array $elements 
+     * 
+     * @return \Vision\Form\AbstractCompositeType Provides a fluent interface.
+     */
     public function addElements(array $elements) 
     {
-		foreach ($elements as $element) {
-			$this->addElement($element);
-		}
+        foreach ($elements as $element) {
+            $this->addElement($element);
+        }
         return $this;
-	}
+    }
     
+    /**
+     * @param string $name 
+     * 
+     * @return null|mixed
+     */
     public function getElement($name) 
     {
         if (isset($this->elements[$name])) {
             return $this->elements[$name];
         }
-		return null;
-	}
+        return null;
+    }
     
+    /**
+     * @return array
+     */
     public function getElements()
     {
         return $this->elements;
     }
     
+    /**
+     * @return string
+     */
     public function getName()
     {
         return $this->name;
     }   
 
     /**
-     * Add a decorator
+     * @return \Vision\Form\AbstractCompositeType Provides a fluent interface.
      */
-	public function addDecorator(Decorator\DecoratorInterface $decorator) 
+    public function addDecorator(Decorator\DecoratorInterface $decorator) 
     {
         $this->decorators[] = $decorator;
         return $this;
-	}
+    }
     
     /**
-     * Add decorators
+     * @return \Vision\Form\AbstractCompositeType Provides a fluent interface.
      */
-	public function addDecorators(array $decorators) 
+    public function addDecorators(array $decorators) 
     {
         foreach ($decorators as $decorator) {
             $this->addDecorator($decorator);
         }
         return $this;
-	}
-	
-    /**
-     * Get all decorators
-     */
-	public function getDecorators() 
-    {
-		return $this->decorators;
-	}
+    }
     
     /**
-     * Reset decorators
+     * @return array
+     */
+    public function getDecorators() 
+    {
+        return $this->decorators;
+    }
+    
+    /** 
+     * @return \Vision\Form\AbstractCompositeType Provides a fluent interface.
      */
     public function resetDecorators()
     {
