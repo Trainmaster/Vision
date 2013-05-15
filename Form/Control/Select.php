8045913272b5de626c252f61144d5d370262f892
Trainmaster <trainmaster@gmx.net>
2013-05-15 23:25:21 +0200

    Added idb

--- Form/Control/Select.php
+++ Form/Control/Select.php
@@ -1,9 +1,21 @@
 <?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */ 
 namespace Vision\Form\Control;
 
 use Vision\Form\Decorator;
 use Vision\Html\ElementFactory;
 
+/**
+ * Select
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ */ 
 class Select extends MultiOptionControlAbstract 
 {
     protected $tag = 'select';
--- Form/Control/Submit.php
+++ Form/Control/Submit.php
@@ -1,8 +1,20 @@
 <?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */ 
 namespace Vision\Form\Control;
 
 use Vision\Form\Decorator\Label;
 
+/**
+ * Submit
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ */ 
 class Submit extends ControlAbstract 
 {
     protected $tag = 'input';
