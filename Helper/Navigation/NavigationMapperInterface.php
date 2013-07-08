b55002f7aba40a074cc31a17eee253386cd329a4
Trainmaster <trainmaster@gmx.net>
2013-07-08 21:32:38 +0200

    Initial commit

---
+++ Helper/Navigation/NavigationMapperInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */ 
+namespace Vision\Helper\Navigation;
+
+/**
+ * NavigationMapperInterface
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ */
+interface NavigationMapperInterface
+{
+    /**
+     * @api
+     *
+     * @param int $id 
+     * @param int $languageId 
+     */
+    public function loadByIdAndLanguageId($id, $languageId);
+}
