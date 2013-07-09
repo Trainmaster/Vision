ac7f0d49ff1edbba540a31ee6433b74172eea5e0
Trainmaster <trainmaster@gmx.net>
2013-07-09 23:13:33 +0200

    Initial commit

---
+++ Filter/InputString.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+namespace Vision\Filter;
+
+/**
+ * InputString
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ */
+class InputString implements FilterInterface
+{
+    protected $options = array();
+    
+    public function __construct(array $options = array())
+    {
+        $this->options = $options;
+    }
+    
+    public function getOptions()
+    {
+        return $this->options;
+    }
+    
+    public function filter($value)
+    {       
+        return filter_var($value, FILTER_SANITIZE_STRING, $this->getOptions());
+    }
+}
