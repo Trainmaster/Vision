4e55cad8ef8ed25d34af47c0d37391fa8a802a26
Frank Liepert <trainmaster@gmx.net>
2013-09-16 21:08:13 +0200

    Initial commit

---
+++ src/Vision/Cache/CacheAwareInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */ 
+namespace Vision\Cache;
+
+/**
+ * CacheAwareInterface
+ *
+ * @author Frank Liepert
+ */
+interface CacheAwareInterface
+{
+    public function setCache(CacheInterface $cache);
+}
