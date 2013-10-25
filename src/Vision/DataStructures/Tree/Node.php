d07de74d82347054c9c196d5569ab94b450d25f2
Frank Liepert <trainmaster@gmx.net>
2013-10-25 17:07:55 +0200

    Initial commit

---
+++ src/Vision/DataStructures/Tree/Node.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */ 
+namespace Vision\DataStructures\Tree;
+
+/**
+ * Node
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ */
+class Node
+{
+    /** @type null|int $id */
+    protected $id = null;
+    
+    /** @type array $children */
+    protected $children = array();
+    
+    /**
+     * @api
+     * 
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @api
+     * 
+     * @param self $child 
+     * 
+     * @return $this Provides a fluent interface.
+     */
+    public function addChild(self $child)
+    {
+        $this->children[] = $child;
+        return $this;
+    }
+    
+    /**
+     * @api
+     * 
+     * @return array
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+    
+    /**
+     * @api
+     * 
+     * @return bool
+     */
+    public function hasChildren()
+    {
+        return !empty($this->children);
+    }
+    
+    /**
+     * @api
+     * 
+     * @return array
+     */
+    public function resetChildren()
+    {
+        return $this->children = array();
+    }
+}
---
+++ src/Vision/DataStructures/Tree/NodeIterator.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */ 
+namespace Vision\DataStructures\Tree;
+
+/**
+ * @author Frank Liepert
+ */
+class NodeIterator implements \RecursiveIterator 
+{
+    /** @type array $nodes */
+    private $nodes;
+
+    /**
+     * @param Node $node 
+     */
+    public function __construct(Node $node) 
+    {
+        $this->nodes = $node->getChildren();
+    }
+    
+    public function hasChildren()
+    {
+        return $this->current()->hasChildren();
+    }
+    
+    public function getChildren()
+    {
+        return new self($this->current());
+    }   
+    
+    public function current()
+    {
+        return $this->nodes[$this->position];
+    }
+    
+    public function key()
+    {
+        return $this->position;
+    }
+    
+    public function next()
+    {
+        $this->position++;
+    }
+    
+    public function rewind()
+    {
+        $this->position = 0;
+    }   
+    
+    public function valid()
+    {
+        return isset($this->nodes[$this->position]);
+    }
+}
