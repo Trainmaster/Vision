d5a470795d69c4d1822d44ae702128000c4774fa
Frank Liepert <trainmaster@gmx.net>
2013-10-28 00:30:54 +0100

    Initial commit

---
+++ src/Vision/Session/Handler/HandlerInterface.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */ 
+namespace Vision\Session\Handler;
+
+/**
+ * HandlerInterface
+ *
+ * @author Frank Liepert
+ */
+interface HandlerInterface
+{
+    public function close();
+
+    public function destroy($session_id);
+    
+    public function gc($maxlifetime);
+    
+    public function open($save_path, $name);
+    
+    public function read($session_id);
+    
+    public function write($session_id, $session_data);
+}
