c0c2cdc7a5a3aa86e65f0ca27dc37faee6d325e4
Frank Liepert <trainmaster@gmx.net>
2013-10-30 21:58:56 +0100

    Initial commit

---
+++ tests/Validator/ValidatorChainTest.php
@@ -0,0 +1,53 @@
+<?php
+use Vision\Validator;
+
+class ValidatorChainTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->chain = new Validator\ValidatorChain;
+    }
+    
+    public function testAddValidator()
+    {
+        $validator = $this->getMock('\Vision\Validator\ValidatorInterface');
+        $this->chain->add($validator);
+        
+        $this->assertAttributeEquals(array($validator), 'validators', $this->chain);
+        $this->assertInstanceOf('\Vision\Validator\ValidatorChain', $this->chain);
+    }
+    
+    public function testSuccess()
+    {
+        $validator = $this->createTrueValidator();
+        $this->chain->add($validator);
+        
+        $this->assertTrue($this->chain->isValid(''));
+    }
+    
+    public function testFailure()
+    {
+        $validator = $this->createFalseValidator();
+        $this->chain->add($validator);
+        
+        $this->assertFalse($this->chain->isValid(''));
+    }
+    
+    protected function createTrueValidator()
+    {
+        $validator = $this->getMock('\Vision\Validator\ValidatorInterface');
+        $validator->expects($this->once())
+                  ->method('isValid')
+                  ->will($this->returnValue(true));
+        return $validator;
+    }
+    
+    protected function createFalseValidator()
+    {
+        $validator = $this->getMock('\Vision\Validator\ValidatorInterface');
+        $validator->expects($this->once())
+                  ->method('isValid')
+                  ->will($this->returnValue(false));
+        return $validator;
+    }
+}
