98d7387bd1e9228376bf7b4fe90692c81d0db36c
Frank Liepert <trainmaster@gmx.net>
2013-11-02 19:18:48 +0100

    Initial commit

---
+++ tests/Validator/InputNotEmptyStringTest.php
@@ -0,0 +1,32 @@
+<?php
+use Vision\Validator;
+
+class InputNotEmptyStringTest extends \PHPUnit_Framework_TestCase
+{
+    static $validator;
+    
+    public static function setUpBeforeClass()
+    {
+        self::$validator = new Validator\InputNotEmptyString;
+    }
+    
+    public function testSuccess()
+    {
+        $this->assertTrue(self::$validator->isValid(' '));
+        $this->assertTrue(self::$validator->isValid('0'));
+        $this->assertTrue(self::$validator->isValid(array('0')));
+        $this->assertTrue(self::$validator->isValid(array(array('0'))));
+    }
+    
+    public function testFailure()
+    {        
+        $this->assertFalse(self::$validator->isValid(new stdClass));
+        $this->assertFalse(self::$validator->isValid(false));
+        $this->assertFalse(self::$validator->isValid(null));
+        $this->assertFalse(self::$validator->isValid(0));
+        $this->assertFalse(self::$validator->isValid(0.1));
+        $this->assertFalse(self::$validator->isValid(''));
+        $this->assertFalse(self::$validator->isValid(array()));
+        $this->assertFalse(self::$validator->isValid(array(array())));
+    } 
+}
