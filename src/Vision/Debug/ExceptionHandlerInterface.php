8bbdf1c0205fd0108d1eebda8ab5aef502099fe5
Frank Liepert <trainmaster@gmx.net>
2014-01-05 20:49:01 +0100

    Initial commit

---
+++ src/Vision/Debug/ExceptionHandlerInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+namespace Vision\Debug;
+
+/**
+ * ExceptionHandlerInterface
+ *
+ * @author Frank Liepert
+ */
+interface ExceptionHandlerInterface
+{
+    public function handle(\Exception $exception);
+}
