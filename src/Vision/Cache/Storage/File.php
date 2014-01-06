18b2ea6f58c192ca122befec0067c479412a6fef
Frank Liepert <trainmaster@gmx.net>
2014-01-06 16:34:19 +0100

    Fix: remove dot

--- src/Vision/Cache/Storage/File.php
+++ src/Vision/Cache/Storage/File.php
@@ -22,7 +22,7 @@ class File implements StorageInterface
     protected $cacheDirChmod = 0755;
 
     /** @type string $cacheFileExtension */
-    protected $cacheFileExtension = '.cache';
+    protected $cacheFileExtension = 'cache';
 
     /**
      * @param array $options {
