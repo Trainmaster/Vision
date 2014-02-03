766a921fdc5bbfb9c0ad5be5b23364207d233f50
Frank Liepert <trainmaster@gmx.net>
2014-02-03 12:44:40 +0100

    Remove: $clientData

--- src/Vision/File/UploadedFile.php
+++ src/Vision/File/UploadedFile.php
@@ -8,6 +8,8 @@
  */
 namespace Vision\File;
 
+use InvalidArgumentException;
+
 /**
  * UploadedFile
  *
@@ -15,107 +17,20 @@ namespace Vision\File;
  */
 class UploadedFile extends File
 {
-    /** @type array $clientData */
-    protected $clientData = array();
-
+    /**
+     * Constructor
+     *
+     * @param string $filename
+     */
     public function __construct($filename)
     {
         if (!is_uploaded_file($filename)) {
-            throw new \InvalidArgumentException(sprintf(
+            throw new InvalidArgumentException(sprintf(
                 'The file "%s" is not an uploaded file.',
                 $filename
             ));
         }
 
         parent::__construct($filename);
-    }
-
-    /**
-     * @api
-     *
-     * @param array $data
-     *
-     * @return UploadedFile Provides a fluent interface.
-     */
-    public function setClientData(array $data)
-    {
-        $this->clientData = $data;
-        return $this;
-    }
-
-    /**
-     * @api
-     *
-     * @return string|null
-     */
-    public function getClientName()
-    {
-        if (isset($this->clientData['name'])) {
-            return $this->clientData['name'];
-        }
-        return null;
-    }
-
-    /**
-     * @api
-     *
-     * @return string|null
-     */
-    public function getClientType()
-    {
-        if (isset($this->clientData['type'])) {
-            return $this->clientData['type'];
-        }
-        return null;
-    }
-
-    /**
-     * @api
-     *
-     * @return string|null
-     */
-    public function getClientSize()
-    {
-        if (isset($this->clientData['size'])) {
-            return $this->clientData['size'];
-        }
-        return null;
-    }
-
-    /**
-     * @api
-     *
-     * @return string|null
-     */
-    public function getClientTmpName()
-    {
-        if (isset($this->clientData['tmp_name'])) {
-            return $this->clientData['tmp_name'];
-        }
-        return null;
-    }
-
-    /**
-     * @api
-     *
-     * @return string|null
-     */
-    public function getClientError()
-    {
-        if (isset($this->clientData['error'])) {
-            return $this->clientData['error'];
-        }
-        return null;
-    }
-
-    /**
-     * @api
-     *
-     * @return string
-     */
-    public function getClientExtension()
-    {
-        $extension = pathinfo($this->getClientName(), PATHINFO_EXTENSION);
-        return $extension;
     }
 }
