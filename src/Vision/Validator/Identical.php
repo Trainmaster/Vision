60ddf16c608f1a34aef176be98781366797f3015
Frank Liepert <trainmaster@gmx.net>
2014-03-22 15:31:01 +0100

    Initial commit

---
+++ src/Vision/Validator/Identical.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2013 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+namespace Vision\Validator;
+
+/**
+ * Identical
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ */
+class Identical extends AbstractValidator
+{
+    /** @type string NOT_IDENTICAL */
+    const NOT_IDENTICAL = 'The given operands are not identical.';
+
+    /** @type mixed $operand */
+    protected $operand = null;
+
+    /**
+     * @param mixed $operand
+     */
+    public function __construct($operand = null)
+    {
+        $this->setoperand($operand);
+    }
+
+    /**
+     * @api
+     *
+     * @param mixed $operand
+     *
+     * @return $this Provides a fluent interface.
+     */
+    public function setOperand($operand)
+    {
+        $this->operand = $operand;
+        return $this;
+    }
+
+    /**
+     * @api
+     *
+     * @return mixed
+     */
+    public function getOperand()
+    {
+        return $this->operand;
+    }
+
+    /**
+     * @api
+     *
+     * @param string $value
+     *
+     * @return bool
+     */
+    public function isValid($value)
+    {
+        $this->resetErrors();
+
+        if ($value === $this->operand) {
+            return true;
+        }
+
+        $this->addError(self::NOT_IDENTICAL);
+        $this->addError(array(
+            'operand1' => $value,
+            'operand2' => $this->operand
+        ));
+
+        return false;
+    }
+}
---
+++ tests/Validator/IdenticalTest.php
@@ -0,0 +1,57 @@
+<?php
+use Vision\Validator;
+
+class IdenticalTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructEmpty()
+    {
+        $validator = new Validator\Identical;
+        $this->assertNull($validator->getOperand());
+    }
+
+    public function testConstructWithOperand()
+    {
+        $validator = new Validator\Identical('foo');
+        $this->assertSame('foo', $validator->getOperand());
+    }
+
+    public function testSetAndGetOperand()
+    {
+        $validator = new Validator\Identical;
+        $this->assertNull($validator->getOperand());
+
+        $validator->setOperand('foo');
+        $this->assertSame('foo', $validator->getOperand());
+    }
+
+    public function testSuccess()
+    {
+        $validator = new Validator\Identical;
+        $this->assertTrue($validator->isValid(null));
+
+        $validator->setOperand('');
+        $this->assertTrue($validator->isValid(''));
+
+        $validator->setOperand('foo');
+        $this->assertTrue($validator->isValid('foo'));
+
+        $validator->setOperand(1);
+        $this->assertTrue($validator->isValid(1));
+
+        $object = new stdClass;
+        $validator->setOperand($object);
+        $this->assertTrue($validator->isValid($object));
+    }
+
+    public function testFailure()
+    {
+        $validator = new Validator\Identical;
+        $this->assertFalse($validator->isValid(''));
+
+        $validator->setOperand(1);
+        $this->assertFalse($validator->isValid('1'));
+
+        $validator->setOperand(new stdClass);
+        $this->assertFalse($validator->isValid(new stdClass));
+    }
+}
