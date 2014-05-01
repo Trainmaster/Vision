9bffcc6d7a13c8269db50131353b60a614cb4410
Frank Liepert <trainmaster@gmx.net>
2014-05-01 18:35:08 +0200

    Initial commit

---
+++ src/Vision/Authentication/ResultInterface.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Vision PHP-Framework
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ * @copyright 2012-2014 Frank Liepert
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+namespace Vision\Authentication;
+
+/**
+ * Authentication
+ *
+ * @author Frank Liepert <contact@frank-liepert.de>
+ */
+interface ResultInterface
+{
+    /**
+     * @api
+     *
+     * @return bool
+     */
+    public function isSuccess();
+
+    /**
+     * @api
+     *
+     * @return mixed
+     */
+    public function getIdentity();
+}
