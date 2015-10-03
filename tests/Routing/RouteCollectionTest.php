4c23e50744d2adb0c72a32f88c3506beef871713
Frank Liepert <trainmaster@gmx.net>
2015-10-03 21:29:22 +0200

    Add unit tests

---
+++ tests/Routing/RouteCollectionTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace VisionTest\Routing;
+
+use Vision\Routing\RouteCollection;
+
+class RouteCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddAndGet()
+    {
+        $httpMethod = 'GET';
+        $path = '/';
+        $controller = 'foo';
+
+        $collection = new RouteCollection();
+        $collection->add($httpMethod, $path, $controller);
+
+        $this->assertCount(1, $collection->getAll());
+
+        $route = $collection->get('/');
+
+        $this->assertInstanceOf('Vision\Routing\Route', $route);
+        $this->assertSame([$httpMethod], $route->getHttpMethod());
+        $this->assertSame($path, $route->getPath());
+        $this->assertSame($controller, $route->getController());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Route "/" has already been defined.
+     */
+    public function testAddDuplicateThrowsException()
+    {
+        $collection = new RouteCollection();
+        $collection->add('GET', '/', 'foo');
+        $collection->add('GET', '/', 'foo');
+    }
+}
