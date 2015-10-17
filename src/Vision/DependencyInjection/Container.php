5309138e37accd915a8b860adb967c2af8681302
Frank Liepert <trainmaster@gmx.net>
2015-10-17 22:41:37 +0200

    Resolve possible dependencies

--- src/Vision/DependencyInjection/Container.php
+++ src/Vision/DependencyInjection/Container.php
@@ -237,7 +237,7 @@ class Container implements ContainerInterface
             $factoryInstance = $this->resolveReference($factory[0]);
 
             return $factory[2]
-                ? call_user_func_array([$factoryInstance, $factory[1]], $factory[2])
+                ? call_user_func_array([$factoryInstance, $factory[1]], $this->resolveDependencies($factory[2]))
                 : $factoryInstance->$factory[1]();
         }
 
