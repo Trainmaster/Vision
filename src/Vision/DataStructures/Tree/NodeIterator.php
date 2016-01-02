4175426fdc937a63e7c8cb40b02cb16dd970c2eb
Frank Liepert <trainmaster@gmx.net>
2016-01-02 18:28:10 +0100

    Bump year

--- src/Vision/DataStructures/Tree/NodeIterator.php
+++ src/Vision/DataStructures/Tree/NodeIterator.php
@@ -3,7 +3,7 @@
  * Vision PHP-Framework
  *
  * @author Frank Liepert <contact@frank-liepert.de>
- * @copyright 2012-2015 Frank Liepert
+ * @copyright 2012-2016 Frank Liepert
  * @license http://www.opensource.org/licenses/mit-license.php MIT
  */
 namespace Vision\DataStructures\Tree;
