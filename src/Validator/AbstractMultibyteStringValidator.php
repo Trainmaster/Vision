9751a9852cb64baa3fd8e53adefa93f5e61cdb15
Frank Liepert <trainmaster@gmx.net>
2017-01-26 22:44:33 +0100

    Remove PHPDocs

--- src/Validator/AbstractMultibyteStringValidator.php
+++ src/Validator/AbstractMultibyteStringValidator.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Vision PHP-Framework
- *
- * @author Frank Liepert <contact@frank-liepert.de>
- * @copyright 2012-2016 Frank Liepert
- * @license http://www.opensource.org/licenses/mit-license.php MIT
- */
 namespace Vision\Validator;
 
 abstract class AbstractMultibyteStringValidator extends AbstractValidator
