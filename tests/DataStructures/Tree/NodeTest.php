70470f637e0c29fdfa5dcde5fc0bca375b601d6c
Frank Liepert <trainmaster@gmx.net>
2017-02-23 22:50:26 +0100

    Add tests for Node

---
+++ tests/DataStructures/Tree/NodeTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace VisionTest\DataStructures\Tree;
+
+use PHPUnit\Framework\TestCase;
+use Vision\DataStructures\Tree\Node;
+use Vision\DataStructures\Tree\NodeInterface;
+
+class NodeTest extends TestCase {
+
+    public function testParent() {
+        $nodeA = new Node;
+        $nodeB = (new Node)->setParent($nodeA);
+
+        $this->assertSame($nodeA, $nodeB->getParent());
+    }
+
+    public function testAddChild() {
+        $nodeA = new Node;
+        $nodeB = (new Node)->addChild($nodeA);
+
+        $this->assertSame($nodeB->getChildren(), [$nodeA]);
+    }
+
+    public function testdAddChildShouldSetParent() {
+        $nodeA = new Node;
+        $nodeB = (new Node)->addChild($nodeA);
+
+        $this->assertSame($nodeA->getParent(), $nodeB);
+    }
+
+    public function testGetChildren() {
+        $node = new Node;
+        $this->assertSame($node->getChildren(), []);
+
+        $node->addChild(new Node);
+        $this->assertContainsOnlyInstancesOf(NodeInterface::class, $node->getChildren());
+        $this->assertCount(1, $node->getChildren());
+    }
+
+    public function testRemoveChild() {
+        $nodeA = new Node;
+        $nodeB = (new Node)->addChild($nodeA);
+        $nodeB->removeChild($nodeA);
+
+        $this->assertEmpty($nodeB->getChildren());
+    }
+
+    public function testHasChildren() {
+        $node = new Node;
+        $this->assertSame($node->hasChildren(), false);
+
+        $node->addChild(new Node);
+        $this->assertSame($node->hasChildren(), true);
+    }
+}
